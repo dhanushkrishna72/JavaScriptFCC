298b3b541aa4651979e48ecd3db9432cf2f73eaf
dhanush K <dhanushkrishna2402@icloud.com>
2025-12-19 17:27:02 +0530

    I am updating the code, after astarting the DOM manipulation on 19th/Dec

--- prompt/index.js
+++ prompt/index.js
@@ -2,15 +2,12 @@ const btn = document.getElementById("prompt-btn");
 const output = document.getElementById("output");
 btn.addEventListener("click", () => {
     const userName = prompt("Enter your name", "guest");
-    output.textContent = `Hello ${userName}!!`;
+    
+    // below code is update on 19th/dec, to showcase the dom manupilation.
+    output.innerHTML = `<i style = "color: red;">${output.textContent = `Hello ${userName}!!`}</i>`;
 });
 
 
 const colorBtn = document.getElementById("color-btn");
-
-colorBtn.addEventListener("click", () => {
-    document.body.style.backgroundColor = `#00664d`;
-    output.textContent = "Helloooo Worlllldddddd.....!!!!!!!";
-});
 
 
