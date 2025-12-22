658be41a575f24d441db61c33006cae19beedfae
dhanush K <dhanushkrishna2402@icloud.com>
2025-12-22 22:01:26 +0530

    DOM manipulations

---
+++ domChallenge/index.js
@@ -0,0 +1,12 @@
+let h1 = document.querySelector("h1");
+
+h1.style.textDecoration = ("underline wavy #23ed");
+
+h1.style.transitionDelay = ("3ms");
+
+h1.style.background = "red";
+
+
+let p = document.querySelector("p");
+
+p.innerHTML = "<b>Updated</b> by JavaScript";
