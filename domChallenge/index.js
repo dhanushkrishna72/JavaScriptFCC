f1f69c89e2fb4307f45ec858380bde3f50b4f377
dhanush K <dhanushkrishna2402@icloud.com>
2025-12-22 22:42:20 +0530

    Implement list creation and reset functionality with buttons

--- domChallenge/index.js
+++ domChallenge/index.js
@@ -10,3 +10,30 @@ h1.style.background = "red";
 let p = document.querySelector("p");
 
 p.innerHTML = "<b>Updated</b> by JavaScript";
+
+
+let ul = document.querySelector("ul");
+let li = document.querySelectorAll("li");
+
+
+let btn_re = document.getElementById("btn-reset");
+let btn_ce = document.getElementById("btn-create");
+
+btn_ce.addEventListener("click", () => {
+    let number = prompt("Enter the number of values you need: ");
+
+    for (let i = 0; i < number; i++) {
+        let item = prompt("Enter the fruit Name:");
+        let lis = document.createElement("li");
+        lis.textContent = item;
+        ul.appendChild(lis);
+    }
+})
+
+btn_re.addEventListener("click", () => {
+    if (ul.getElementsByTagName('li').length > 0) {
+        ul.remove("ul");
+    } else {
+        alert("List is empty, Create a list first");
+    }
+})
