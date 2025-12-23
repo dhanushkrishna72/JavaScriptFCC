6941377d2a720d6400551b5c29244b2544b7533c
dhanush K <dhanushkrishna2402@icloud.com>
2025-12-23 22:04:39 +0530

    finishing Events

--- EventHandling/index.js
+++ EventHandling/index.js
@@ -1,8 +1,40 @@
+// let h1 = document.querySelector("h1");
+
+// h1.addEventListener('click', () => {
+//     h1.style.color = "Blue"
+//     h1.style.fontSize = "142px";
+//     h1.style.textAlign = "center";
+//     h1.textContent = "Clicked";
+// })
+
+
+// let input = document.querySelector("input");
+
+// function myFunc(evt) {
+//     console.log(evt.data);
+// }
+
+// input.addEventListener("input", myFunc);
+
+
+// let selec = document.querySelector("select");
+
+// selec.addEventListener("change", (ele) => {
+//     let el = ele.target.value;
+//     if (el === "apple") {
+//         document.body.style.backgroundColor = "Red";
+
+//     } else {
+//         document.body.style.background = "blue";
+//     }
+// });
+
 let h1 = document.querySelector("h1");
 
-h1.addEventListener('click', () => {
-    h1.style.color = "Blue"
-    h1.style.fontSize = "142px";
-    h1.style.textAlign = "center";
-    h1.textContent = "Clicked";
+window.addEventListener("keydown", (ele) => {
+    if (ele.key === " ") {
+        h1.textContent = "SPACE";
+    } else {
+        h1.textContent = ele.key;
+    }
 })
